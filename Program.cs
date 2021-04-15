99acf7a717e12378a3449b9bf0ba07d897015304
Katerina-ops-18 <61978136+Katerina-ops-18@users.noreply.github.com>
2021-04-15 10:30:44 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Lab_1_1
+{
+    class Program
+    {
+        delegate double Anonim(double A, double B, double C);
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Перше число -->");
+            double A = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Друге число -->");
+            double B = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Третє число -->");
+            double C = Convert.ToInt32(Console.ReadLine());
+
+            Anonim del = (a,b,c) => (double)(A + B + C) / 3;
+            Console.WriteLine("Середне арифметичне введених чисел {0:##.###}", del(A,B,C));
+
+            Console.ReadLine();
+        }
+    }
+}
